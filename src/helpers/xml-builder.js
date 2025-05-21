248cdf07a20bc5b9a43583567c815b895945348f
mikebausordev <74281865+mikebausordev@users.noreply.github.com>
2025-05-22 05:42:22 +0800

    Set default table margin to 0

--- src/helpers/xml-builder.js
+++ src/helpers/xml-builder.js
@@ -1642,7 +1642,7 @@ const buildTableProperties = (attributes) => {
       }
     });
   }
-  const tableCellMarginFragment = buildTableCellMargins(160);
+  const tableCellMarginFragment = buildTableCellMargins(0);
   tablePropertiesFragment.import(tableCellMarginFragment);
 
   // by default, all tables are center aligned.
